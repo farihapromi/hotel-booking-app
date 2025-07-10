9f0a6b139a30d53f64e814cb620bf32f5af1e5bf
Fariha Promi <farihapromi2611@gmail.com>
2025-07-10 12:26:49 +0600

    Add react router

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-blue-400'>WElcvome</h1>
+      <p className="bg-red-400">Hotel</p>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './app'
+import {BrowserRouter} from 'react-router-dom'
+
+
+createRoot(document.getElementById('root')).render(
+
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  
+
+)
+
