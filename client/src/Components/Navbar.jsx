4ee73a056a7a93ff902dbd0b0d817eb1c699f122
Fariha Promi <farihapromi2611@gmail.com>
2025-07-12 10:57:48 +0600

    Add mobile device

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import React from 'react';
 import searchIcon from "../assets/searchIcon.svg";
 import logo from "../assets/logo.svg"
+import menuIcon from "../assets/menuIcon.svg"
+import closeIcon from "../assets/closeIcon.svg"
 
 <img src={searchIcon} alt="Search" />
 
@@ -52,7 +54,7 @@ const Navbar = () => {
 
                 {/* Desktop Right */}
                 <div className="hidden md:flex items-center gap-4">
-                    <img src={searchIcon}/>
+                    <img src={searchIcon} alt="src" className={` ${isScrolled && "invert "} h-7 transition-all duration-500`}/>
                     <button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
                         Login
                     </button>
@@ -60,20 +62,14 @@ const Navbar = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
-                    <svg onClick={() => setIsMenuOpen(!isMenuOpen)} className={`h-6 w-6 cursor-pointer ${isScrolled ? "invert" : ""}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                        <line x1="4" y1="6" x2="20" y2="6" />
-                        <line x1="4" y1="12" x2="20" y2="12" />
-                        <line x1="4" y1="18" x2="20" y2="18" />
-                    </svg>
+                   <img onClick={()=>setIsMenuOpen(!isMenuOpen)}
+                   src={menuIcon} alt="menu" className={`${isScrolled&& "invert"}h-4`}/>
                 </div>
 
                 {/* Mobile Menu */}
                 <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                        <svg className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
+                       <img src={closeIcon} alt="closeIcon" className='h-6.5'/>
                     </button>
 
                     {navLinks.map((link, i) => (
