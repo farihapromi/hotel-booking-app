af20cfbdf37fbc45d3ed97846a76d7989aa02c5f
Fariha Promi <farihapromi2611@gmail.com>
2025-07-12 11:31:11 +0600

    Add bg image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   const isOwnerPath=useLocation().pathname.includes('admin')
@@ -8,6 +9,11 @@ const App = () => {
     <div>
       {/* condiitonal rendering of navabr.if it is not admin then show this navbar */}
       {!isOwnerPath &&  <Navbar/>}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
 
       
     </div>
---
+++ client/src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div
+      className="h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center"
+      style={{ backgroundImage: 'url("/heroImage.png")' }}
+    >
+      
+    </div>
+  );
+};
+
+export default Hero;
