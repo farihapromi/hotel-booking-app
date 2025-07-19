b916d0d84ed0c660c337bbd97ea3400020faf665
Fariha Promi <farihapromi2611@gmail.com>
2025-07-19 23:09:24 +0600

    Add room details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './Pages/Home'
-import Footer from './Components/Footer'
-import AllRooms from './Pages/AllRooms'
-
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './Pages/Home';
+import Footer from './Components/Footer';
+import AllRooms from './Pages/AllRooms';
+import RoomDetails from './Pages/RoomDetails';
 
 const App = () => {
-  const isOwnerPath=useLocation().pathname.includes('admin')
+  const isOwnerPath = useLocation().pathname.includes('admin');
   return (
     <div>
       {/* condiitonal rendering of navabr.if it is not admin then show this navbar */}
-      {!isOwnerPath &&  <Navbar/>}
-      <div className="min-h-[70vh]">
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-            <Route path='/rooms' element={<AllRooms/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails />} />
         </Routes>
       </div>
-  <Footer/>
-
-      
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
