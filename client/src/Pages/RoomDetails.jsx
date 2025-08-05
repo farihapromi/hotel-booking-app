e6f25704595aca5da51fc13b5701defd2d603f91
Fariha Promi <farihapromi2611@gmail.com>
2025-08-05 22:46:02 +0600

    Add style for button

--- client/src/Pages/RoomDetails.jsx
+++ client/src/Pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { assets, roomsDummyData } from '../assets/assets';
+import { assets, facilityIcons, roomsDummyData } from '../assets/assets';
 import StarRating from '../Components/StarRating';
 
 const RoomDetails = () => {
@@ -54,12 +54,87 @@ const RoomDetails = () => {
                   src={image}
                   alt='room image'
                   className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${
-                    mainImage === image && 'outline-3 outline-orange-300'
+                    mainImage === image && 'outline-3 outline-orange-500'
                   }`}
                 />
               ))}
           </div>
         </div>
+        {/* room hightlights */}
+        <div className='flex flex-col md:flex-row md:justify-between mt-10'>
+          <div>
+            <h1 className='font-playfair text-3xl md:text-4xl'>
+              Experinece Luxary Like Never Before
+            </h1>
+            <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
+              {room.amenities.map((item, index) => (
+                <div
+                  key={index}
+                  className='flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100'
+                >
+                  <img
+                    src={facilityIcons[item]}
+                    alt='item'
+                    className='w-5 h-5'
+                  />
+                  <p className='text-xs'>{item}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* Room Price */}
+          <p className='text-2xl font-medium'>${room.pricePerNight}/night</p>
+        </div>
+        {/* Check in check out form */}
+        <form
+          className='flex flex-col md:flex-row items-start md:item-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'
+          action=''
+        >
+          <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+            <div className='flex flex-col'>
+              <label htmlFor='CheckIn' className='font-medium'>
+                Check-In
+              </label>
+              <input
+                type='date'
+                id='checkInDate'
+                placeholder='Check-In'
+                className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none'
+                required
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor='CheckOut' className='font-medium'>
+                Check-Out
+              </label>
+              <input
+                type='date'
+                id='checkOutDate'
+                placeholder='Check-Out'
+                className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none'
+                required
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor='guests' className='font-medium'>
+                Guests
+              </label>
+              <input
+                type='number'
+                id='guests'
+                placeholder='0'
+                className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none'
+                required
+              />
+            </div>
+          </div>
+          <button
+            type='submit'
+            className='bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-[26px] py-3 md:py-4 text-base cursor-pointer'
+          >
+            Book Now
+          </button>
+        </form>
       </div>
     )
   );
