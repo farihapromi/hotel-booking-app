e904d2d6960e8cf4275869ff6fdd255986edef27
Fariha Promi <farihapromi2611@gmail.com>
2025-08-15 11:13:45 +0600

    Add booking data

--- client/src/Pages/MyBookings.jsx
+++ client/src/Pages/MyBookings.jsx
@@ -17,6 +17,29 @@ const MyBookings = () => {
           <div className='w-1/3'>Date & Timings</div>
           <div className='w-1/3'>Payment</div>
         </div>
+        {bookings.map((booking) => (
+          <div
+            key={booking._id}
+            className='grid grid-cols-1 md:grid-cols-[3fr_2f_1fr] w-full border-b border-gray-300 py-6 firts-border-t'
+          >
+            {/* Hotel details */}
+            <div>
+              <img
+                src={booking.room.images[0]}
+                alt='hotel-image'
+                className='min-md:w-44 rounded shadow object-cover'
+              />
+              <div>
+                <p>{booking.hotel.name}</p>
+                <span>({booking.room.roomType})</span>
+              </div>
+            </div>
+            {/* Date & Timings */}
+            <div></div>
+            {/* Payment */}
+            <div></div>
+          </div>
+        ))}
       </div>
     </div>
   );
