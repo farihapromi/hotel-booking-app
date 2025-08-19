53cb549ab6231df8ea550956af60f2196e161062
Fariha Promi <farihapromi2611@gmail.com>
2025-08-19 22:35:37 +0600

    Add admin dashboard

--- client/src/Components/hotelOwner/Navbar.jsx
+++ client/src/Components/hotelOwner/Navbar.jsx
@@ -1,6 +1,7 @@
 import { UserButton } from '@clerk/clerk-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
   return (
