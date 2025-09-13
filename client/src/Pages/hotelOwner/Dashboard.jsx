2581c92c3a3dfe6e3768eebad55a3e141b0d4349
Fariha Promi <farihapromi2611@gmail.com>
2025-09-13 21:27:01 +0600

    Add dashboard data

--- client/src/Pages/hotelOwner/Dashboard.jsx
+++ client/src/Pages/hotelOwner/Dashboard.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Title from '../../Components/Title';
+
+import { assets, dashboardDummyData } from '../../assets/assets';
 
 const Dashboard = () => {
-  return <div></div>;
+  const [dashBoard, setDashboardData] = useState(dashboardDummyData);
+  return (
+    <div>
+      <Title
+        align='left'
+        font='outlet'
+        title='Dashboard'
+        subTitle='Monitor Your room listing ,track bookings and analyze revnue-all  in one place.Stay updated with real-time insights to ensure smooth operations .'
+      />
+      <div className='flex gap-4 my-8'>
+        {/* total bookingd */}
+        <div className='bg-primary/3 border border-primary/10 flex rounded p-4 pr-8'>
+          <img
+            src={assets.totalBookingIcon}
+            alt=''
+            className='max-sm:hidden h-10'
+          />
+          <div>
+            <p>Total Bookings</p>
+            <p></p>
+          </div>
+        </div>
+        {/* Total Revenue */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
