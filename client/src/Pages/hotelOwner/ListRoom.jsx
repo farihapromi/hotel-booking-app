bc8bfaaf3492f1e20a468885bfb29d8b587c0a83
Fariha Promi <farihapromi2611@gmail.com>
2025-10-10 22:05:51 +0600

    Add table data of lkisted room

--- client/src/Pages/hotelOwner/ListRoom.jsx
+++ client/src/Pages/hotelOwner/ListRoom.jsx
@@ -29,6 +29,41 @@ const ListRoom = () => {
               </th>
             </tr>
           </thead>
+          {/* table body */}
+          <tbody className='text-sm'>
+            {rooms.map((item, index) => (
+              <tr key={index}>
+                <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                  {item.roomType}
+                </td>
+                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden'>
+                  {item.amenities.join(',')}
+                </td>
+                <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                  {item.pricePerNight}
+                </td>
+                <td className='py-3 px-4 text-red-500 border-t border-gray-300 text-center'>
+                  <label
+                    htmlFor=''
+                    className='relative inline-flex items-center cursor-pointer text-gray-900 gap-3'
+                  >
+                    <input
+                      type='checkbox'
+                      name=''
+                      id=''
+                      className='sr-only peer'
+                      checked={item.isAvailable}
+                    />
+                    <div className='w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-700 transition-colors duration-200'></div>
+                    <span
+                      className='dot absolute left-1 top-1 h-5 w-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-
+                    x-5'
+                    ></span>
+                  </label>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
