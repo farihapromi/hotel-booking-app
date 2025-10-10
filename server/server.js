edc2cf6508dfb21a24f1a20e039c94252a6cacbf
Fariha Promi <farihapromi2611@gmail.com>
2025-10-10 23:38:20 +0600

    Configure celrk

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
+
 connectDB();
 const app = express();
 app.use(cors()); //connect backend to frontend
+app.use(clerkMiddleware());
+
 app.get('/', (req, res) => res.send('Api is Working'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
