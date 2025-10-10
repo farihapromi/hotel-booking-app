f3f59ef06475b9ba11e83d6ae4b92875857f3632
Fariha Promi <farihapromi2611@gmail.com>
2025-10-10 22:25:07 +0600

    Add .env file

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+const app = express();
+app.use(cors()); //connect backend to frontend
+app.get('/', (req, res) => res.send('Api is Working'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
