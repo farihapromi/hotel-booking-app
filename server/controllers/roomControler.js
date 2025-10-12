2ac65123a992065e7cf18c29a4e17ff767b8e10f
Fariha Promi <farihapromi2611@gmail.com>
2025-10-12 22:16:11 +0600

    Cretae roomController

---
+++ server/controllers/roomControler.js
@@ -0,0 +1,26 @@
+export const createRoom = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+//api to get all room
+export const getRooms = async (req, res) => {
+  try {
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getOwnerRooms = async (req, res) => {
+  try {
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Api to toggle rooim avaiablity
+export const toggleRoomAvaiablity = async (req, res) => {
+  try {
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
