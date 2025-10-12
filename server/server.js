db7a2951b7920fc993db884804e83d8064020ddb
Fariha Promi <farihapromi2611@gmail.com>
2025-10-12 11:35:46 +0600

    Add vercel comfiguration

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express';
+import clerkWebhooks from './controllers/clerkWebhooks.js';
 
 connectDB();
 const app = express();
@@ -10,6 +11,8 @@ app.use(cors()); //connect backend to frontend
 //middleware
 app.use(express.json());
 app.use(clerkMiddleware());
+//API to lsten clerk webhook
+app.use('/api/clerk', clerkWebhooks);
 
 app.get('/', (req, res) => res.send('Api is Working'));
 const PORT = process.env.PORT || 3000;
