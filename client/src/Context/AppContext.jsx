5ca2863fb206187eb24d3c47ed918281685633cb
Fariha Promi <farihapromi2611@gmail.com>
2025-10-13 19:14:04 +0600

    Add elements for context

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -1,5 +1,18 @@
 import axios from 'axios';
-import { createContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser, useAuth } from '@clerk/clerk-react';
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 const AppContext = createContext();
-export const AppProvider = ({ children }) => {};
+export const AppProvider = ({ children }) => {
+  const currency = import.meta.env.VITE_CURRENCY || '$';
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { getToken } = useAuth;
+  const [isOwner, setIsOwner] = useState(false);
+  const [showHotel, setShowHotelReg] = useState(false);
+
+  const value = {};
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+export const useAppContext = () => useContext(AppContext);
