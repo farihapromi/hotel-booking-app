e6af2633b213945af512d72da8056c97c575aed0
Fariha Promi <farihapromi2611@gmail.com>
2025-10-21 21:51:29 +0600

    add send mail options

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,6 +1,7 @@
 import Booking from '../models/Booking.js';
 import Room from '../models/Room.js';
 import Hotel from '../models/Hotel.js';
+import transporter from '../configs/nodemailer.js';
 
 const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
   try {
@@ -67,6 +68,31 @@ export const createBooking = async (req, res) => {
       checkOutDate,
       totalPrice,
     });
+    //nodemailer email send part
+    //Uncomment this while snwding email request
+    // const mailOptions = {
+    //   from: process.env.SENDER_EMAIL,
+    //   to: req.user.email,
+    //   subjec: 'Hotel Booking Details',
+    //   html: `
+    //   <h2>Your Booking Details </h2>
+    //   <p> Dear ${req.user.username}, </P>
+    //   <p>Thank you for booking ! Here are your details: </P>
+    //   <ul>
+    //   <li><strong>Booking ID :</strong> ${booking._id}</li>
+    //   <li><strong>Hotel Name :</strong> ${roomData.hotel.name}</li>
+    //    <li><strong>Location :</strong> ${roomData.hotel.address}</li>
+    //   <li><strong>Date :</strong> ${booking.checkInDate.toDateString()}</li>
+    //   <li><strong>Booking Amount :</strong>${process.env.CURRENCY || '$'} ${
+    //     booking.totalPrice
+    //   } / night</li>
+    //   </ul>
+    //   <p>We look forward to welcome you ! </p>
+    //   <p> If you need to make any changes ,feel free to contact us. </p>
+    //   `,
+    // };
+
+    // await transporter.sendMail(mailOptions);
     res.json({ success: true, booking });
   } catch (error) {
     console.log(error);
